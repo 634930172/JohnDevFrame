4be62f267f8f00d18c72d637b6302fd212f4b186
John <634930172@qq.com>
2019-01-22 15:20:05 +0800

    [USER]去除无用代码

--- app/src/main/java/com/john/johndevframe/LeakApplication.java
+++ app/src/main/java/com/john/johndevframe/LeakApplication.java
@@ -1,7 +1,6 @@
 package com.john.johndevframe;
 
 import android.app.Application;
-import android.content.Context;
 
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
